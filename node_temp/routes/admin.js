e7828009944750a10fdc6261219505b128a1793d
lyg <1983654762@qq.com>
2018-08-24 11:32:51 +0800

    commit

--- node_temp/routes/admin.js
+++ node_temp/routes/admin.js
@@ -2,8 +2,8 @@ var express = require('express')
 
 var admin = express.Router()
 
-router.get('/', function (req, res) {
-  res.end("admin/admin!!!!")
+admin.get('/admin/login', function (req, res) {
+  res.end("admin/login!!!!")
 })
 
 module.exports = admin
